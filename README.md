ee6c59d645c13abaac3adda96fe3ab9dd29b7703
Zahid Kizmaz <tech@zahid.rocks>
2024-07-29 22:07:42 +0200

    feat: add flake.nix

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ Generate docker-compose.nix via:
 ```shell
 nix run github:aksiksi/compose2nix -- -project=unbound -root_path=.
 ```
+
+OR
+
+```shell
+nix develop
+compose2nix -project=unbound -root_path=.
+```
--- flake.nix
+++ flake.nix
@@ -3,16 +3,23 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    compose2nix.url = "github:aksiksi/compose2nix";
   };
 
-  outputs = { nixpkgs }:
-    {
-      packages = {
-        default = nixpkgs.lib.mkShell {
-          buildInputs = [
-            (import ./docker-compose.nix)
+  outputs = { nixpkgs, flake-utils, compose2nix, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        nixosModules.app = import ./docker-compose.nix;
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.nixd
+            pkgs.nixpkgs-fmt
+            compose2nix.packages.${system}.default
           ];
         };
-      };
-    };
+      });
 }
