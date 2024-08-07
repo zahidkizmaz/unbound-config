9632cfb49c7e1912d22ab73aeaead65b0e1f4c8b
Zahid Kizmaz <tech@zahid.rocks>
2024-08-07 19:57:45 +0200

    remove config

--- docker-compose.nix
+++ docker-compose.nix
@@ -1,35 +1,32 @@
 { pkgs, ... }:
 
 {
-  config = {
-    # Runtime
-    virtualisation.podman = {
-      enable = true;
-      autoPrune.enable = true;
-      dockerCompat = true;
-      defaultNetwork.settings = {
-        # Required for container networking to be able to use names.
-        dns_enabled = true;
-      };
+  # Runtime
+  virtualisation.podman = {
+    enable = true;
+    autoPrune.enable = true;
+    dockerCompat = true;
+    defaultNetwork.settings = {
+      # Required for container networking to be able to use names.
+      dns_enabled = true;
     };
-    virtualisation.oci-containers.backend = "podman";
+  };
+  virtualisation.oci-containers.backend = "podman";
 
-    environment.systemPackages = [ pkgs.podman-compose ];
+  environment.systemPackages = [ pkgs.podman-compose ];
 
-    networking.firewall.allowedTCPPorts = [ 8553 ];
+  networking.firewall.allowedTCPPorts = [ 8553 ];
 
-    systemd.services.unbound-compose = {
-      path = with pkgs; [ podman podman-compose ];
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = true;
-        ExecStart = "podman-compose -f ${./docker-compose.yml} up";
-        ExecStop = "podman-compose -f ${./docker-compose.yml} down";
-        Restart = "always";
-      };
-      wantedBy = [ "multi-user.target" ];
-      after = [ "podman.service" "podman.socket" ];
-      requires = [ "podman.service" "podman.socket" ];
+  systemd.services.unbound-compose = {
+    path = with pkgs; [ podman podman-compose ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = "podman-compose -f ${./docker-compose.yml} up";
+      ExecStop = "podman-compose -f ${./docker-compose.yml} down";
     };
+    wantedBy = [ "multi-user.target" ];
+    after = [ "podman.service" "podman.socket" ];
+    requires = [ "podman.service" "podman.socket" ];
   };
 }
