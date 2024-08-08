c932224b96b23f1ceee942b5ed68cf306c35f59b
Zahid Kizmaz <tech@zahid.rocks>
2024-08-08 23:43:33 +0200

    try compose2nix

---
+++ docker-compose-service.nix
@@ -0,0 +1,41 @@
+{ pkgs, ... }:
+
+let
+  composeFilePath = ./docker-compose.yml;
+  podmanPath = "${pkgs.podman}/bin/podman";
+  podmanComposePath = "${pkgs.podman-compose}/bin/podman-compose";
+in
+{
+  # Runtime
+  virtualisation.podman = {
+    enable = true;
+    autoPrune.enable = true;
+    dockerCompat = true;
+    defaultNetwork.settings = {
+      # Required for container networking to be able to use names.
+      dns_enabled = true;
+    };
+  };
+  virtualisation.oci-containers.backend = "podman";
+
+  environment.systemPackages = [ pkgs.podman-compose ];
+
+  networking.firewall.allowedTCPPorts = [ 8553 ];
+
+  systemd.services.unbound-compose = {
+    path = with pkgs; [ podman podman-compose ];
+    serviceConfig = {
+      Restart = "always";
+      RemainAfterExit = true;
+      ExecStartPre = [
+        "${podmanPath} pod rm -f pod_store || true"
+        "${podmanComposePath} --verbose -f ${composeFilePath} build"
+      ];
+      ExecStart = "${podmanComposePath} -f ${composeFilePath} up --force-recreate";
+      ExecStop = "${podmanComposePath} -f ${composeFilePath} down";
+    };
+    wantedBy = [ "multi-user.target" ];
+    after = [ "podman.service" "podman.socket" ];
+    requires = [ "podman.service" "podman.socket" ];
+  };
+}
